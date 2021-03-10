b3e2df9739dfce51dc78d07508a0f9ae9fc65708
minkyungyeee <minkfree94@gmail.com>
2021-03-11 01:27:26 +0900

    Update 2021-03-11 01:27

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,17 +15,36 @@
 
         },
         section1Fn:function(){
+            var $win = $(window);
+            var $winW = $(window).width();
+            var $winH = $(window).height();
+            var $slide = $('#section1 .slide');
+            var $sec1 = $('#section1');
+
             var $slideWrap = $('#section1 .slide-wrap');
             var $nextBtn = $('#section1 .next-btn');
             var $prevBtn = $('#section1 .prev-btn');
             var cnt = 0;
             var n = $('#section1 .slide').length-2;
 
+            function resizeFn(){
+                $winW = $(window).width();
+                $winH = $(window).height();
+                $slide.css({width:$winW, height:$winH});
+                $sec1.css({width:$winW, height:$winH});
+                $slideWrap.stop().animate({left:-1903*cnt},0);
+            }
+
+            resizeFn();
+            
+            $win.resize(function(){
+                resizeFn();
+            });
             function mainSlideFn(){
-                $slideWrap.stop().animate({left:-1903*cnt},600,function(){
+                $slideWrap.stop().animate({left:-$winW*cnt},600,function(){
                     if(cnt>n-1){cnt=0}
                     if(cnt<0){cnt=n-1}
-                    $slideWrap.stop().animate({left:-1903*cnt},0);
+                    $slideWrap.stop().animate({left:-$winW*cnt},0);
                 });
             }
             function nextSlideCountFn(){
