0e88f2edc3ee3b23c4a993bfba2dc61f9a35c5f7
minkyungyeee <minkfree94@gmail.com>
2021-03-29 12:09:53 +0900

    21-03-29 최종

--- js/jump.js
+++ js/jump.js
@@ -4,6 +4,13 @@ function goUrl(z){
             location.href = '';
             break
 
+        case 'insta':
+            window.open('https://www.instagram.com');
+            break
+
+        case 'demo':
+            location.href = 'https://lithohtml.themezaa.com/home-furniture-shop.html';
+            break
         default:
             alert();
     }
